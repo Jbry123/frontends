008db0636283cb9371367689e19b0fdc61a22a51
Jonathan Bry <jonathanbry@comcast.net>
2018-05-19 12:54:52 -0700

    got a SPA with a sign in, used the same vuetify template as you with firebase

---
+++ wyvern/src/components/Home.vue
@@ -0,0 +1,28 @@
+<style>
+
+.flex-text-xs-center-xs12 mt-5 {
+    background-color: 'red';
+    font-size: '13px';
+  
+}
+</style>
+
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="header-div" mt-5>
+        <h1>Home page</h1>
+      </v-flex>
+      <v-flex xs12 class="p-div" mt-3>
+        <div>
+        <p>This is a user's home page</p>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
